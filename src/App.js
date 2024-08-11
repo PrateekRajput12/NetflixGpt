842a70339227005e6ae27e509d1384845f2f3db1
PrateekRajput12 <ficredinn@gmail.com>
2024-08-11 06:46:35 +0530

    Starting of Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Body from './Components/Body';
+function App() {
+  return (
+    <div className="App">
+<Body></Body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Body.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { RouterProvider,createBrowserRouter ,Route} from 'react-router-dom'
+import Header from './Header'
+
+const Body = () => {
+    const body=createBrowserRouter({
+         
+    })
+  return (
+    <div className='font-bold text-gray-800 text-center'>Body</div>
+  )
+}
+
+
+export default Body
